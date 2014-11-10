5c7e0cc6a5cb8689e5b707352a84a69a07265505
Antoine Frénoy <frenoy@gmx.com>
2014-11-10 17:55:22 +0100

    Correctly list replicate folders

--- averageallrelatedness.sh
+++ averageallrelatedness.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+cd $1
+
+nb=`ls -d * |wc -l`
+
 for g in {1..50}
 do
   m=0
@@ -8,7 +12,7 @@ do
     v=`head -n ${g} ${i}/Rsummary |tail -n 1`
     m=`scale=6;echo $m+$v|bc`
   done
-  echo "scale=6;$m/25"|bc >> Rbygen
+  echo "scale=6;$m/${nb}"|bc >> Rbygen
 done
 
 for g in {1..50}
@@ -19,6 +23,6 @@ do
     v=`head -n ${g} ${i}/Rsummary_1 |tail -n 1`
     m=`scale=6;echo $m+$v|bc`
   done
-  echo "scale=6;$m/25"|bc >> Rbygen_1
+  echo "scale=6;$m/${nb}"|bc >> Rbygen_1
 done
 
--- computeallrelatedness.sh
+++ computeallrelatedness.sh
@@ -2,10 +2,10 @@
 
 cd ${1}
 
-for rep in `ls -d 3*`
+for rep in `ls -d *`
 do
   cd ${rep}
-  ~/code/aevol4.3/src/post_treatments/aevol_misc_parsemutations -b 0 -e 4000 -g 500 -f 500
+  ~/code/aevol4.3/src/post_treatments/aevol_misc_parsemutations -b 5000 -e 10000 -g 500 -f 500 -r 0
   for file in `ls relatedness*`
   do
     awk '{ sum += $1; n++ } END { if (n > 0) printf("%.6f\n", sum / n); }' $file >> Rsummary
